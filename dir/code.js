fee8b1d2051189e9105a501dea442d8e117db644
OlgaTitarenko <kizuneworld@gmail.com>
2020-09-22 10:58:06 +0300

    add distortion

--- dir/code.js
+++ dir/code.js
@@ -1,25 +1,60 @@
+
+let firstTime = -1;
+
 function cursor() {
 
     const link = document.querySelectorAll('h2 > .hover-this');
     const cursor = document.querySelector('.cursor');
+    const elements = document.querySelectorAll('.element');
 
-    function moveCursor(e) {
-
-        const { pageX: x, pageY: y } = e;
-        cursor.style.left = x + 'px';
-        cursor.style.top = y + 'px';
-
-    }
+    
     function animateit(e) {
-        const item = this;
         cursor.classList.add("cursor-hover");
+
+        const index = e.target.dataset.item;
+
+
+        if (e.type === 'mousemove') {
+            if (elements[index].classList.contains('hide')) {
+                elements[index].classList.remove('hide')
+            }
+            if (firstTime != -1 && firstTime != index) {
+                elements[index].classList.add('show');
+            }
+            elements[index].classList.add('active');
+
+        }
         if (e.type === 'mouseleave') {
             cursor.classList.remove("cursor-hover");
+            if (firstTime != -1) {
+                elements[firstTime].classList.add('hide');
+                elements[index].classList.remove('show');
+            }
+            elements[index].classList.remove('active');
+
         }
+        firstTime = index;
     }
+
+    function moveCursor(e) {
+        const { pageX: x, pageY: y } = e;
+        if (!e.target.classList.contains('hover-this')) {
+            if (firstTime !== -1) {
+                elements[firstTime].classList.remove('hide');
+                firstTime = -1;
+            }
+            
+        }
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
+    
+    }
+
     link.forEach(b => b.addEventListener('mousemove', animateit));
     link.forEach(b => b.addEventListener('mouseleave', animateit));
     window.addEventListener('mousemove', moveCursor);
+
+
 };
 
 cursor();
@@ -38,3 +73,6 @@ function showPage(event) {
     
     //console.log(event.target.dataset.page);
 }
+link.forEach(b => b.removeEventListener('mousemove', animateit));
+link.forEach(b => b.removeEventListener('mouseleave', animateit));
+window.removeEventListener('mousemove', moveCursor);
