c4961740d3da70a3d034f752c2c19a59c005d3f5
OlgaTitarenko <kizuneworld@gmail.com>
2020-10-10 09:56:22 +0300

    about page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,82 +1,122 @@
-let firstTime = -1;
-const link = document.querySelectorAll('h2 > .hover-this');
-const cursor = document.querySelector('.cursor');
-const elements = document.querySelectorAll('.element');
+'use strict';
 
-function animateit(e) {
-    cursor.classList.add("cursor-hover");
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),
+            new Route('about', 'about.html'),
+            new Route('works', 'works.html')
+        ]);
 
-    const index = e.target.dataset.item;
-    const hoverVideo = elements[index].querySelector('video');
+    }
+    init();
 
-    if (e.type === 'mousemove') {
-        if (elements[index].classList.contains('hide')) {
-            elements[index].classList.remove('hide')
+    function homeFunction() {
+        let firstTime = -1;
+        const main = app.querySelector('main');
+
+        const link = main.querySelectorAll('h2 > .hover-this');
+        const elements = main.querySelectorAll('.element');
+
+        function animateit(e) {
+            cursor.classList.add("cursor-hover");
+    
+            const index = e.target.dataset.item;
+            const hoverVideo = elements[index].querySelector('video');
+    
+            if (e.type === 'mousemove') {
+                if (elements[index].classList.contains('hide')) {
+                    elements[index].classList.remove('hide')
+                }
+                if (firstTime != -1 && firstTime != index) {
+                    elements[index].classList.add('show');
+                }
+                elements[index].classList.add('active');
+                if (hoverVideo) {
+                    hoverVideo.play();
+                }
+            }
+            if (e.type === 'mouseleave') {
+                cursor.classList.remove("cursor-hover");
+                if (firstTime != -1) {
+                    elements[firstTime].classList.add('hide');
+                    elements[index].classList.remove('show');
+                }
+                elements[index].classList.remove('active');
+                if (hoverVideo) {
+                    hoverVideo.pause();
+                    hoverVideo.currentTime = 0;
+                }
+    
+    
+            }
+            firstTime = index;
         }
-        if (firstTime != -1 && firstTime != index) {
-            elements[index].classList.add('show');
+
+        function moveCursorHome (e) {
+            if (!e.target.classList.contains('hover-this')) {
+                if (firstTime !== -1) {
+                    elements[firstTime].classList.remove('hide');
+                    firstTime = -1;
+                }
+    
+            }
         }
-        elements[index].classList.add('active');
-        if (hoverVideo) {
-            hoverVideo.play();
+        window.removeEventListener('mousemove', moveCursorHome);
+        link.forEach(b => b.removeEventListener('mousemove', animateit));
+        link.forEach(b => b.removeEventListener('mouseleave', animateit));
+
+
+        link.forEach(b => b.addEventListener('mousemove', animateit));
+        link.forEach(b => b.addEventListener('mouseleave', animateit));
+        window.addEventListener('mousemove', moveCursorHome);
+    }
+
+    function checkPage() {
+        const main = app.querySelector('main');
+        if (main.classList.contains('home-container')) {
+            homeFunction();
         }
     }
-    if (e.type === 'mouseleave') {
-        cursor.classList.remove("cursor-hover");
-        if (firstTime != -1) {
-            elements[firstTime].classList.add('hide');
-            elements[index].classList.remove('show');
-        }
-        elements[index].classList.remove('active');
-        if (hoverVideo) {
-            hoverVideo.pause();
-            hoverVideo.currentTime = 0;
-        }
+
+    const app = document.getElementById('app');
+    const cursor = document.querySelector('.cursor');
+    app.removeEventListener('DOMNodeInserted', checkPage);
+    app.addEventListener('DOMNodeInserted', checkPage);
+
+    function moveCursor(e) {
+        const { pageX: x, pageY: y } = e;
        
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
 
     }
-    firstTime = index;
-}
+    function mouse() {
+        window.addEventListener('mousemove', moveCursor);
+    };
+    window.removeEventListener('mousemove', moveCursor);
 
-function moveCursor(e) {
-    const { pageX: x, pageY: y } = e;
-    if (!e.target.classList.contains('hover-this')) {
-        if (firstTime !== -1) {
-            elements[firstTime].classList.remove('hide');
-            firstTime = -1;
-        }
+    mouse();
+
+}());
         
-    }
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-
-}
-
-function mouse() {
-
-    link.forEach(b => b.addEventListener('mousemove', animateit));
-    link.forEach(b => b.addEventListener('mouseleave', animateit));
-    window.addEventListener('mousemove', moveCursor);
 
 
-};
-
-mouse();
-
+/*
 function showPage(event) {
     const pages = document.querySelectorAll('main');
     pages.forEach(page =>  {if (event.target.dataset.page === page.dataset.page) {
-        page.hidden=false; 
+        page.hidden=false;
         page.classList.add('slideInUp')
     } else {
         page.hidden=true;
         page.classList.remove('slideInUp');
     }
     });
-        
-    
+
+
     //console.log(event.target.dataset.page);
-}/*
+}
 link.forEach(b => b.removeEventListener('mousemove', animateit));
 link.forEach(b => b.removeEventListener('mouseleave', animateit));
 window.removeEventListener('mousemove', moveCursor);*/
