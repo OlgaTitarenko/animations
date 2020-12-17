2f283841b62ab351806b3faca8a2757d8e1b6dca
OlgaTitarenko <kizuneworld@gmail.com>
2020-12-17 07:57:42 +0200

    fix fonts

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # animations
+opening local 
 
 node server.js 
-open localhost:5060
+open localhost:5000
+
+on vps 
+1. go to cd ../var/www/html
+2. stop apache 
+sudo service apatch2 stop
+3. restart server
+pm2 restart server.js
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/build", express.build(path.resolve(__dirname, "build")));
+app.use("/build", express.static(path.resolve(__dirname, "build")));
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"));
