e883248d31befebbeb8e4381813529f013ea964d
OlgaTitarenko <kizuneworld@gmail.com>
2020-12-21 15:50:59 +0200

    fix parallax

--- build/js/views/About.js
+++ build/js/views/About.js
@@ -6,15 +6,6 @@ export default class extends AbstractView {
     }
     mainFunction = () =>  {
        this.cursorAnimation();
-
-        const imageP = document.querySelector('#parallax-img');
-        const parallaxImg = () => {
-            const value = window.scrollY;
-            imageP.style.top = 0.6*value + 'px';
-        }
-       
-        window.removeEventListener('scroll',parallaxImg);
-        window.addEventListener('scroll',parallaxImg);
        
     }
     
@@ -22,60 +13,67 @@ export default class extends AbstractView {
     async getHtml() {
         return `
         <main class="about-container slideInUp" data-page="about">
-        <div class="about-image">
-            <img src="./build/assets/img/about.jpg" alt="" id='parallax-img'>
-        </div>
+
+<div class='wrapper'>
+      <section class="section parallax bg1">
+        <div class='about-info'>
         <div class="about-text">
-            <p><span class="about-text-space"></span>Vitali Senevych is
-            the digital designer interested
-            in pop-culture, alternatives, music
-            and motion.
-            </p>
-            <p><span class="about-text-space"></span>He creates web & apps services, interactive and visual experience to fill that empty space. He works at the Alty, where he collaborates with equally passionate people on saucy projects.    
-            </p>
+        <p><span class="about-text-space"></span>Vitali Senevych is
+          the digital designer interested
+          in pop-culture, alternatives, music
+          and motion.
+        </p>
+        <p><span class="about-text-space"></span>He creates web & apps services, interactive and visual experience to fill that empty space. He works at the Alty, where he collaborates with equally passionate people on saucy projects.    
+        </p>
+      </div>
+      <div class="about-text">
+        <h2><span class="about-text-space"></span>Awards & Achievements
+        </h2>
+        <p>
+          behance — pop tube
+        </p>
+        <p>
+          csswinner — kish personal site
+        </p>
+        <p>
+          uplabs.best in web — mountains
+        </p>
+        <p>
+          onepagelove.featured — kish personal site
+        </p>
+        <p>
+          behance.interaction — quick slim
+        </p>
+        <p>
+          indigoawards — kish personal site
+        </p>
+      </div>
+      <div class="about-text about-text_links">
+        <h2><span class="about-text-space"></span>Contacts
+        </h2>
+        <p>
+          <a href="mailto:vs@senevych.design" target="_blank"> vs@senevych.design </a>
+        </p>                
+        <p>
+          <a href="https://www.instagram.com/v.senevych/" target="_blank">Instagram</a>, 
+          <a href="https://www.linkedin.com/in/senevych/" target="_blank">LinkedIn</a>, 
+          <a href="https://www.facebook.com/senevych/" target="_blank">Facebook</a>, 
+          <a href="https://www.behance.net/senevych" target="_blank">Behance</a>, 
+          <a href="https://dribbble.com/senevych" target="_blank">Dribbble</a>, 
+          <a href="https://vimeo.com/senevych" target="_blank">Vimeo</a>
+        </p>
+        <p>
+          Kyiv, Ukraine
+        </p>
+      </div>
         </div>
-        <div class="about-text">
-            <h2><span class="about-text-space"></span>Awards & Achievements
-            </h2>
-            <p>
-                behance — pop tube
-            </p>
-            <p>
-                csswinner — kish personal site
-            </p>
-            <p>
-                uplabs.best in web — mountains
-            </p>
-            <p>
-                onepagelove.featured — kish personal site
-            </p>
-            <p>
-                behance.interaction — quick slim
-            </p>
-            <p>
-                indigoawards — kish personal site
-            </p>
-        </div>
-        <div class="about-text about-text_links">
-            <h2><span class="about-text-space"></span>Contacts
-            </h2>
-            <p>
-                <a href="mailto:vs@senevych.design" target="_blank"> vs@senevych.design </a>
-            </p>                
-            <p>
-                <a href="https://www.instagram.com/v.senevych/" target="_blank">Instagram</a>, 
-                <a href="https://www.linkedin.com/in/senevych/" target="_blank">LinkedIn</a>, 
-                <a href="https://www.facebook.com/senevych/" target="_blank">Facebook</a>, 
-                <a href="https://www.behance.net/senevych" target="_blank">Behance</a>, 
-                <a href="https://dribbble.com/senevych" target="_blank">Dribbble</a>, 
-                <a href="https://vimeo.com/senevych" target="_blank">Vimeo</a>
-            </p>
-            <p>
-                Kyiv, Ukraine
-            </p>
-    
-            
-        </div>
+      </section>
+</div>
+
+
+        
+        
+        
     </main>
         `;
     }
